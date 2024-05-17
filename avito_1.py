ef4dd233e930c850b068a3b7b3315012ca8d6a06
gbrs <gbrs@mail.ru>
2024-05-17 22:51:43 +0300

    correct input in task 1

--- avito_1.py
+++ avito_1.py
@@ -1,3 +1,4 @@
+'''
 a = 1
 b = 2
 c = 3
@@ -5,6 +6,11 @@ d = 4
 k = 0
 m = 2
 N = 1_000
+'''
+
+a, b, c, d = map(int, input().split())
+k, m = map(int, input().split())
+N = int(input())
 
 step = (m - k) / N
 integral = 0
@@ -15,4 +21,8 @@ for i in range(N):
 
 print(round(integral, 3))
 
-
+'''
+1 2 3 4
+0 2
+1000
+'''
