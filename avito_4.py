aaa8125d7db493eb1607eb919f94122abb9b8378
gbrs <gbrs@mail.ru>
2024-05-20 08:34:34 +0300

    start to solve task 4

---
+++ avito_4.py
@@ -0,0 +1,35 @@
+from math import sin, cos
+
+def dist(x, a):
+    ans = (
+        2 * (cos(x) - a * sin(x))**2 +
+        (sin(x) - a * cos(x))**2
+    )
+    return ans
+
+K, N = 0.001, 4
+R, M, T = 2, 2000, 1000
+
+for i in range(1, N):
+    r, m, t = map(int, input().split())
+    if (0.9*R <= r <= 1.1*R) and (0.95*M <= m <= 1.05*M) and (0.98*T <= t <= 1.02*T):
+        left = 0.0
+        right = 2.0 * 10 ** 38
+        for _ in range(100):
+            middle = (left + right) / 2
+            if dist(middle, i) > 0:
+                right = middle
+            else:
+                left = middle
+        print(middle)
+        break
+else:
+    print(-1)
+
+
+
+'''
+2 2000 1010
+3 2100 950
+4 2200 1100
+'''
