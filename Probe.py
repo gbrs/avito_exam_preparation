bda3e8fd5d6b4a5feb4a7c4e3ab4e53277f8560a
gbrs <gbrs@mail.ru>
2024-05-22 00:09:04 +0300

    continue to solve task 4

--- Probe.py
+++ Probe.py
@@ -1,1 +1,16 @@
-import scipy.stats as sps
+from math import cos
+
+def dist(x):
+    print(cos(x))
+    return cos(x)
+
+left = 0
+right = 1.5
+for _ in range(10):
+    middle = (left + right) / 2
+    if dist(middle) > 0:
+        right = middle
+    else:
+        left = middle
+
+print(middle)
--- avito_4.py
+++ avito_4.py
@@ -14,13 +14,13 @@ for i in range(1, N):
     r, m, t = map(int, input().split())
     if (0.9*R <= r <= 1.1*R) and (0.95*M <= m <= 1.05*M) and (0.98*T <= t <= 1.02*T):
         left = 0.0
-        right = 2.0 * 10 ** 38
+        right = 26.0
         for _ in range(100):
             middle = (left + right) / 2
             if dist(middle, i) > 0:
-                right = middle
-            else:
                 left = middle
+            else:
+                right = middle
         print(middle)
         break
 else:
