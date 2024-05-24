bde21e6e81fbab01f0e47160b1ab83e346b08a12
gbrs <gbrs@mail.ru>
2024-05-24 08:11:27 +0300

    create example for determinator computing

---
+++ determinate.py
@@ -0,0 +1,14 @@
+def compute_determinator(x: list) -> int:
+    return 42
+
+
+lst = [
+    [1, 1, 1, 1],
+    [2, 1, 3, 1],
+    [1, 2, 3, 0],
+    [-1, 2, 0, 1]
+]
+# -6
+
+print(compute_determinator(lst))
+
