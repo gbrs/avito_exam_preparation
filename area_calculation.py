465cbc45d8aa149a31dd10d15ec2f8b1e37f24cb
gbrs <gbrs@mail.ru>
2024-06-06 20:20:48 +0300

    compute a area

--- area_calculation.py
+++ area_calculation.py
@@ -1,6 +1,22 @@
+from cross_product_2d import cross_product
+
 # dots = ((1, 5), (3, 5), (4, 3), (6, 3), (2, 1), (2, 3))  # 8
 
 dots = ((-3, -3), (-2, 3), (0, 3), (2, 1), (2, -1), (1, -2))  # 21.5
+
+
+# нахождение площади через векторные произведения
+
+area = 0
+for i in range(1, len(dots)):
+    area += cross_product(dots[i], dots[i - 1])
+
+area += cross_product(dots[0], dots[-1])
+
+print(abs(area / 2))
+
+
+# нахождение площади через трапеции
 
 area = 0
 for i in range(1, len(dots)):
@@ -9,3 +25,6 @@ for i in range(1, len(dots)):
 area += (dots[0][1] + dots[-1][1]) / 2 * (dots[0][0] - dots[-1][0])
 
 print(abs(area))
+
+
+
---
+++ cross_product_2d.py
@@ -0,0 +1,30 @@
+def cross_product(a, b):
+    return a[0]*b[1] - a[1]*b[0]
+
+if __name__ == '__main__':
+    v1 = tuple(map(int, input().split()))
+    v2 = tuple(map(int, input().split()))
+    print(cross_product(v1, v2))
+
+'''
+2 -1
+1 3
+7
+
+1 3
+2 -1
+-7
+
+2 16
+3 19
+-10
+
+2 -16
+3 19
+86
+
+2 -16
+-3 -19
+-86
+'''
+
