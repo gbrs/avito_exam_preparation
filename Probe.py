b78e6854fe50c3ada6662611ace62c3fdfbbbb5d
gbrs <gbrs@mail.ru>
2024-06-08 00:14:07 +0300

    start Mann-Whitney test

--- Probe.py
+++ Probe.py
@@ -1,16 +1,47 @@
-from math import cos
+group_1 = [
+    [1, 'КТИ', 112],
+    [1, 'ВСИ', 105],
+    [1, 'МНИ', 109],
+    [1, 'АНМ', 90],
+    [1, 'УРА', 130],
+    [1, 'ВФЫ', 117],
+    [1, 'РКИ', 117],
+    [1, 'ТРИ', 125],
+    [1, 'ТРК', 134],
+    [1, 'ТНК', 109]
+]
 
-def dist(x):
-    print(cos(x))
-    return cos(x)
+group_2 = [
+    [2, 'БРИ', 121],
+    [2, 'ДРО', 120],
+    [2, 'РНА', 134],
+    [2, 'ВРА', 119],
+    [2, 'ГРА', 115],
+    [2, 'ДЖА', 106],
+    [2, 'ВЦК', 107],
+    [2, 'ЮЕР', 101],
+    [2, 'ЖЕН', 97],
+    [2, 'КОР', 117]
+]
 
-left = 0
-right = 1.5
-for _ in range(10):
-    middle = (left + right) / 2
-    if dist(middle) > 0:
-        right = middle
+# 47.5
+
+group = list(group_1)
+group.extend(list(group_2))
+
+group.sort(key=lambda x: x[2])
+
+current = group[0][2]
+counter = 1
+
+for i in range(1, len(group)):
+    if group[i][2] == current:
+        counter += 1
     else:
-        left = middle
+        avg_rang = 3 * i
+        for j in range(i - counter, i):
+            group.append()
 
-print(middle)
+print(*group)
+
+
